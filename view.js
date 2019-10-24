cb35378415f53dc5ac3a8ae28f8c542e256c0653
ipadorusa <ghkfkdeoa092!>
2019-10-24 09:59:58 +0900

    git change commit

--- view.js
+++ view.js
@@ -2,8 +2,9 @@ import { qs } from './help.js';
 
 export default class View {
     constructor() {
-        this.todoList = qs('.todolist');
-        this.init();
+      this.input = qs('.new-todo');
+      this.todoList = qs('.todo-list');
+      this.init();
     }
     init() {
         console.log('completed')
